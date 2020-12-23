0b8303e9966d1b71a9d3da412f4335c779ad41d2
masao takayama <m.takayama0929@gmail.com>
2020-12-23 12:58:23 +0000

    Created User's model and implemented some validations.

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative 'boot'
+require 'rails/all'
+Bundler.require(*Rails.groups)
+module InstagramClone
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.load_defaults 5.2
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+  end
+end
