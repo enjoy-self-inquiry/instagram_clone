592587037f657c52f9d78e5a3d038aa25c00d65f
masao takayama <m.takayama0929@gmail.com>
2020-12-28 02:10:33 +0000

    Added functions(posting with ImageUploader).

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_130010) do
+ActiveRecord::Schema.define(version: 2020_12_28_011707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
