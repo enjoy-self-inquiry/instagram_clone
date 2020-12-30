40127d8491926eff98d0a2189fca479bd3d36562
masao takayama <m.takayama0929@gmail.com>
2020-12-30 13:33:10 +0000

    Implemented PostsHelper. Changed views(Tantetively, similar to facebook_clone.)

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorites_users, through: :favorites, source: :user
+  validates :content, presence: true, length: { maximum: 50}
+  validates :image, presence: true
   mount_uploader :image, ImageUploader
 end
