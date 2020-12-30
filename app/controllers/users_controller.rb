917f7235b24eefdff4357c1553dd51ccfb52c82a
masao takayama <m.takayama0929@gmail.com>
2020-12-30 05:05:30 +0000

    Implemented the function for editting own profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+  @user = User.find_by(id: params[:id])
+  if @user.update(user_params)
+    redirect_to user_path
+  else
+    render :new
+  end
+end
   private
   def user_params
     params.require(:user).permit(:name, :email, :image, :image_cache,
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
   resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
