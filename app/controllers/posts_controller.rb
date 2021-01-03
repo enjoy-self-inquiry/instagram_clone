37fd075e838a0744fc9f8580dd4dc63664628849
masao takayama <m.takayama0929@gmail.com>
2021-01-03 10:53:09 +0000

    Revised posts_controller.rb(line 37).

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
     respond_to do |format|
       if @post.save
-        ConfirmMailer.confirm_mail(@post).deliver
+        #ConfirmMailer.confirm_mail(@post).deliver
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
